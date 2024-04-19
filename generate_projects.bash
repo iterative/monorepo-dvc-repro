288f5010d66e5fc286ef0b2de86a09e25bdf8ecb
Tibor Mach <tibor@iterative.ai>
2024-04-19 14:52:42 +0200

    better naming

--- generate_projects.bash
+++ generate_projects.bash
@@ -2,9 +2,8 @@ N=$1
 
 for i in $( seq 1 $N); do
     mkdir -p project-$i
-    cp script.py project-$i/script.py
-    cp dvc.yaml project-$i/dvc.yaml
-    echo " " >> script.py
+    cp script_template.py project-$i/script.py
+    cp dvc_template.yaml project-$i/dvc.yaml
     echo "print('$RANDOM-$RANDOM')" >> project-$i/script.py
     echo "seed: $RANDOM" > project-$i/params.yaml
 done
